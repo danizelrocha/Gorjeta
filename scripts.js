0f6f034d003109add2763562f405a50cfcca8dde
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-26 10:20:45 -0300

    js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function calculateTip() {
+    // Obter valor da conta e qualidade do serviço
+    const billAmount = document.getElementById("billAmount").value;
+    const serviceQuality = document.getElementById("serviceQuality").value;
+  
+    // Validar entrada
+    if (billAmount === "" || serviceQuality == 0) {
+      alert("Por favor, preencha os campos!");
+      return;
+    }
+  
+    // Calcular gorjeta e valor total
+    const tipAmount = billAmount * serviceQuality;
+    const totalAmount = parseFloat(billAmount) + parseFloat(tipAmount);
+  
+    // Exibir resultados
+    document.getElementById("tipAmount").value = "R$" + tipAmount.toFixed(2);
+    document.getElementById("totalAmount").value = "R$" + totalAmount.toFixed(2);
+  }
+  
